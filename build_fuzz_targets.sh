38ce54f0be2c3012b4b1daade556538590ed970c
MashPlant <mashplant@163.com>
2022-04-19 16:00:00 -0400

    build with cmake

--- build_fuzz_targets.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir -p "fuzz_targets"
-
-for src in .code-intelligence/fuzz_targets/*.cpp; do
-    clang++ -DFUZZ=LLVMFuzzerTestOneInput -Iinclude/ -Iexternal/PEGTL/include -std=c++17 -fsanitize=fuzzer -o "fuzz_targets/$(basename "$src" .cpp)" "$src"
-done
